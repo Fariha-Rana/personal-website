cda80af3492f73a9c4a771bd9f6e6103fd758ebf
Fariha-Rana <fariharana.eth@gmail.com>
2024-02-28 18:44:57 +0530

    updated Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Link from "next/link";
 
+const navItems = [
+  { href: "/", text: "Intro" },
+  { href: "/skills", text: "Skills" },
+  { href: "/projects", text: "Projects" },
+  { href: "/contact", text: "Contact" }
+];
+
 function Navbar() {
   return (
-    <nav className="top-0 fixed w-full p-5 font-mono bg-nav z-50 text-gray-500 ">
+    <nav className="top-0 w-full z-50 p-5 font-mono bg-nav text-gray-500">
       <ul className="flex justify-center lg:text-2xl md:text-lg text-sm">
-        {['', 'Skills', 'Projects', 'Contact'].map((page, index) => (
+        {navItems.map((item, index) => (
           <li key={index} className="pr-4 lg:px-10 px-4">
-            <Link href={`/${page.toLowerCase()}`} className="active:underline hover:underline">
-              <b>{page}</b>
+            <Link href={item.href} className="active:underline hover:underline">
+              <b>{item.text}</b>
             </Link>
           </li>
         ))}
