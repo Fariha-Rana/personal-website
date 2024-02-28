6c97c0ef1d92d9461da0c909910fcf88955854a9
Fariha-Rana <fariharana.eth@gmail.com>
2024-02-28 18:38:53 +0530

    updated Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="top-0 fixed w-full p-5 font-mono bg-nav z-50 text-gray-500 ">
       <ul className="flex justify-center lg:text-2xl md:text-lg text-sm">
-        {['Intro', 'Skills', 'Projects', 'Contact'].map((page, index) => (
+        {['', 'Skills', 'Projects', 'Contact'].map((page, index) => (
           <li key={index} className="pr-4 lg:px-10 px-4">
             <Link href={`/${page.toLowerCase()}`} className="active:underline hover:underline">
               <b>{page}</b>
